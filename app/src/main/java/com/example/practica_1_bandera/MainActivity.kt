4b7d54ac0f6fa535a094d4ffc7583274d220dd2f
Kimchigo <ariadp239@gmail.com>
2025-09-12 12:22:25 -0600

    dbdhcdbh

--- app/src/main/java/com/example/practica_1_bandera/MainActivity.kt
+++ app/src/main/java/com/example/practica_1_bandera/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.practica_1_bandera.screens.InicioScreen
 import com.example.practica_1_bandera.ui.theme.Practica_1_BanderaTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +21,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             Practica_1_BanderaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    InicioScreen()
+//                    Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
                 }
             }
         }
--- app/src/main/java/com/example/practica_1_bandera/screens/InicioScreen.kt
+++ app/src/main/java/com/example/practica_1_bandera/screens/InicioScreen.kt
@@ -3,9 +3,11 @@ package com.example.practica_1_bandera.screens
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
 // en un archivo todoo el diseno de mi pantalla de inicio
 @Composable
-fun InicioScreen(){
+fun InicioScreen(Modifier: Modifier){
     Box(){
         Text(text = "Inicio Screen")
     }
