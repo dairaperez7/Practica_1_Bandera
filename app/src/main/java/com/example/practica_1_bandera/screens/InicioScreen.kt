1314ce3f9976cbe8d498e75cfd08f030793422af
Kimchigo <ariadp239@gmail.com>
2025-09-12 12:14:33 -0600

    dbdhcdbh

---
+++ app/src/main/java/com/example/practica_1_bandera/screens/InicioScreen.kt
@@ -0,0 +1,12 @@
+package com.example.practica_1_bandera.screens
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+// en un archivo todoo el diseno de mi pantalla de inicio
+@Composable
+fun InicioScreen(){
+    Box(){
+        Text(text = "Inicio Screen")
+    }
+}
