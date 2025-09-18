29afd290f6ec38dd769610eac34462b961ac8e09
Kimchigo <ariadp239@gmail.com>
2025-09-18 13:56:55 -0600

    dbdhcdbh

--- app/src/main/java/com/example/practica_1_bandera/MainActivity.kt
+++ app/src/main/java/com/example/practica_1_bandera/MainActivity.kt
@@ -201,3 +201,4 @@ fun Japon() {
 //consejo del preview, para ver que el composable al que voy a hacerle preview no debe tener parametros
 //dentro de ese composable debo llamar al otro con los parametros
 //creo un metodo vacio y en ese metodo vacio llamo al que tiene parametros
+//
