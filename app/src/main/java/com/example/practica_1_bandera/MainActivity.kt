ea79adc425b2fed23cadf70158fe4f42a1bbbc69
Kimchigo <ariadp239@gmail.com>
2025-10-07 21:28:03 -0600

    Fernando !

--- app/src/main/java/com/example/practica_1_bandera/MainActivity.kt
+++ app/src/main/java/com/example/practica_1_bandera/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.practica_1_bandera
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Canvas
@@ -33,27 +32,11 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.app.ComponentActivity
 import com.example.practica_1_bandera.screens.InicioScreen
 import com.example.practica_1_bandera.ui.theme.Practica_1_BanderaTheme
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            Practica_1_BanderaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    InicioScreen(modifier= Modifier.padding(paddingValues = innerPadding))
 
-//                    Greeting(
-//                        name = "Android",
-//                        modifier = Modifier.padding(innerPadding)
-//                    )
-                }
-            }
-        }
-    }
-}
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
@@ -72,58 +55,55 @@ fun GreetingPreview() {
     }
 
 }
-@Preview(showBackground = true)
-@Composable
-fun rectangulo() {
-    Box(modifier = Modifier) {
-
-        Canvas(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(0.dp, 20.dp)
-        )
-///row y box// row no tiene forma directa de pasar el padding
-        //el modifier es como la etiqeuta style, darle visual//
-        //hacerla de nuevo, ahora hacer la de japon. hacer el circulo con una clase que h=haga el circulo
-        //en main la bandera de mexico, creo nueva rama de nombre Japon y ahi creo la bandera. Circulo con shape
-        //1er trabajo, bandera de mexico en la main, cambiamos de rama a la nueva y ahi est ala otra. Hacer commit antes de cambiarse de branch. Sin el push (no tan necesario)
-        {
-            //hacerlo con boxes, column/ row y con boxes // image
-
-            drawRect(
-                color = Color.Green,
-                topLeft = Offset(20f, 0f),
-                size = Size(200f, 600f), //para importar debo clickear alt + enter)// f de float//
-                style = Fill,
-            )
-            drawRect(
-                color = Color.White,
-                topLeft = Offset(200f, 0f),
-                size = Size(200f, 600f), //para importar debo clickear alt + enter)// f de float//
-                style = Fill,
-
-                )
-            drawRect(
-                color = Color.Red,
-                topLeft = Offset(400f, 0f),
-                size = Size(200f, 600f), //para importar debo clickear alt + enter)// f de float//
-                style = Fill,
-            )
-        }
-        val image = painterResource(R.drawable.memories)
-        Image(
-            painter = image,
-            contentDescription = "bandderita",
-            modifier = Modifier
-                .size(60.dp)
-                .offset(160.dp, 180.dp)
-        )
-
-    }
-
-
-
-}
+//@Preview(showBackground = true)
+//@Composable
+//fun rectangulo() {
+//    Box(modifier = Modifier) {
+//
+//        Canvas(
+//            modifier = Modifier
+//                .fillMaxSize()
+//                .padding(0.dp, 20.dp)
+//        )
+/////row y box// row no tiene forma directa de pasar el padding
+//        //el modifier es como la etiqeuta style, darle visual//
+//        //hacerla de nuevo, ahora hacer la de japon. hacer el circulo con una clase que h=haga el circulo
+//        //en main la bandera de mexico, creo nueva rama de nombre Japon y ahi creo la bandera. Circulo con shape
+//        //1er trabajo, bandera de mexico en la main, cambiamos de rama a la nueva y ahi est ala otra. Hacer commit antes de cambiarse de branch. Sin el push (no tan necesario)
+//        {
+//            //hacerlo con boxes, column/ row y con boxes // image
+//
+//            drawRect(
+//                color = Color.Green,
+//                topLeft = Offset(20f, 0f),
+//                size = Size(200f, 600f), //para importar debo clickear alt + enter)// f de float//
+//                style = Fill,
+//            )
+//            drawRect(
+//                color = Color.White,
+//                topLeft = Offset(200f, 0f),
+//                size = Size(200f, 600f), //para importar debo clickear alt + enter)// f de float//
+//                style = Fill,
+//
+//                )
+//            drawRect(
+//                color = Color.Red,
+//                topLeft = Offset(400f, 0f),
+//                size = Size(200f, 600f), //para importar debo clickear alt + enter)// f de float//
+//                style = Fill,
+//            )
+//        }
+//        val image = painterResource(R.drawable.memories)
+//        Image(
+//            painter = image,
+//            contentDescription = "bandderita",
+//            modifier = Modifier
+//                .size(60.dp)
+//                .offset(160.dp, 180.dp)
+//        )
+//    }
+//
+//}
 
 @Preview(showBackground = true)
 @Composable
