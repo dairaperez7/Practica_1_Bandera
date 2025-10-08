7f1bb13c28a7ef3732f0710ff29458a43acdf719
Kimchigo <ariadp239@gmail.com>
2025-10-07 20:09:40 -0600

    Fernando !

--- app/src/main/java/com/example/practica_1_bandera/screens/InicioScreen.kt
+++ app/src/main/java/com/example/practica_1_bandera/screens/InicioScreen.kt
@@ -1,65 +1,66 @@
 package com.example.practica_1_bandera.screens
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.asComposePath
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.graphics.shapes.CornerRounding
-import androidx.graphics.shapes.RoundedPolygon
-import androidx.graphics.shapes.toPath
 
-// en un archivo todoo el diseno de mi pantalla de inicio
 @Composable
-fun InicioScreen(modifier: Modifier) {
-    Box(modifier = Modifier.fillMaxSize()) {
-
-        Box(modifier = Modifier
-            .background(Color(0xFF009739))
+fun InicioScreen(modifier: Modifier = Modifier) {
+    Box(
+        modifier = Modifier
             .fillMaxSize()
-        )
+            .background(Color(0xFF009739))
+    ) {
+        Canvas(modifier = Modifier.fillMaxSize()) {
 
+            val path = Path().apply {
+                moveTo(size.width / 2f, 0f)
+                lineTo(size.width, size.height / 2f)
+                lineTo(size.width / 2f, size.height)
+                lineTo(0f, size.height / 2f)
 
-
-
-
-
-        }
-
-    }
-
-            @Preview(showBackground = true, showSystemUi = false)
-            @Composable
-            fun InicioScreenPreview() {
-                InicioScreen(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(Color.Gray)
-                )
             }
+            drawPath(path, color = Color.Yellow)
 
 
+            val circleRadius = size.minDimension * 0.25f
+            drawCircle(
+                color = Color.Blue,
+                radius = circleRadius,
+                center = center
+            )
 
 
+            val arcRect = Rect(
+                center.x - circleRadius,
+                center.y - circleRadius,
+                center.x + circleRadius,
+                center.y + circleRadius
+            )
+            drawArc(
+                color = Color.White,
+                startAngle = 200f,
+                sweepAngle = 140f,
+                useCenter = false,
+                topLeft = arcRect.topLeft,
+                size = arcRect.size,
+                style = Stroke(width = 10f)
+            )
+        }
+    }
+}
 
-//funciones de orden superior //
-
- //metodos de orden superior son los que no estan dentro de una clase//
-//no afecta del orden de los factores
+@Preview(showBackground = true)
+@Composable
+fun PreviewInicioScreen() {
+    InicioScreen()
+}
